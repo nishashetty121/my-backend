0a3a5c0ff67fccdc57d55f9d46cb867dc249d10e
Nisha Shetty <nishauaday03@gmail.com>
2026-07-18 21:31:34 +0530

    switch to postgresql

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # Connect to database
-engine = create_engine("sqlite:///tasks.db")
+import os
+DATABASE_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 # Database table structure
 class Task(SQLModel, table=True):
